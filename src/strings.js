98f6289d5ccbfc23b3d21294558793f86b07243a
Will Hodgkinson <willhodgkinson5@gmail.com>
2022-06-02 16:14:17 +0100

    Completed numbers and strings tests

--- src/strings.js
+++ src/strings.js
@@ -1,26 +1,29 @@
-function sayHello (string) {
-  // your code here
-};
+function sayHello(string) {
+  return `Hello, ${string}!`;
+}
 
-function uppercase (string) {
-  // your code here
-};
+function uppercase(string) {
+  return string.toUpperCase();
+}
 
-function lowercase (string) {
-  // your code here
-};
+function lowercase(string) {
+  return string.toLowerCase();
+}
 
-function countCharacters (string) {
-  // your code here
-};
+function countCharacters(string) {
+  return string.length;
+}
 
-function firstCharacter (string) {
-  // your code here
-};
+function firstCharacter(string) {
+  return string.charAt(0);
+}
 
-function firstCharacters (string, n) {
-  // your code here
-};
+function firstCharacters(string, n) {
+  if (string.length > 4) {
+    return string.substring(0, 4);
+  }
+  return string.substring(0, 2);
+}
 
 module.exports = {
   sayHello,
